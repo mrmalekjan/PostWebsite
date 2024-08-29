defed85b56ada468c42e069c6b26eef97ad34690
mrmalekjan <malak1377mm@gmail.com>
2024-08-29 20:56:03 +0330

    some visual improvements!!!

---
+++ translations.py
@@ -0,0 +1,20 @@
+translations = {
+    'en': {
+        'language': 'en',
+        'home': 'Home',
+        'profile': 'Profile',
+        'create_post': 'Create A Post',
+        'logout': 'Logout',
+        'login': 'Login',
+        'latest_posts': 'Latest Posts',
+    },
+    'fa': {
+        'language': 'fa',
+        'home': 'خانه',
+        'profile': 'پروفایل',
+        'create_post': 'ایجاد پست',
+        'logout': 'خروج',
+        'login': 'ورود',
+        'latest_posts': 'جدیدترین پست‌ها',
+    }
+}
